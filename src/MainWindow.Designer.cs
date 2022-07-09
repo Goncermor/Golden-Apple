496d0ccf6e7d7846519f8f90de520e35cf1eb999
Gonçalo Morais <goncalocerto9morais@gmail.com>
2022-07-09 19:26:41 +0100

    Adding first files

---
+++ src/MainWindow.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Golden_Apple
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.NotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.NotifyIconMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.checkUpdatesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NotifyIconMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.MarqueeAnimationSpeed = 10;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(337, 17);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // NotifyIcon
+            // 
+            this.NotifyIcon.ContextMenuStrip = this.NotifyIconMenu;
+            this.NotifyIcon.Text = "Golden Apple Alfa 0.0";
+            this.NotifyIcon.Visible = true;
+            // 
+            // NotifyIconMenu
+            // 
+            this.NotifyIconMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkUpdatesToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.NotifyIconMenu.Name = "NotifyIconMenu";
+            this.NotifyIconMenu.Size = new System.Drawing.Size(154, 48);
+            // 
+            // checkUpdatesToolStripMenuItem
+            // 
+            this.checkUpdatesToolStripMenuItem.Name = "checkUpdatesToolStripMenuItem";
+            this.checkUpdatesToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.checkUpdatesToolStripMenuItem.Text = "Check Updates";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 17);
+            this.Controls.Add(this.progressBar1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainWindow";
+            this.Text = "Golden Apple Alfa 0.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.Shown += new System.EventHandler(this.MainWindow_Shown);
+            this.NotifyIconMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ProgressBar progressBar1;
+        private NotifyIcon NotifyIcon;
+        private ContextMenuStrip NotifyIconMenu;
+        private ToolStripMenuItem checkUpdatesToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
---
+++ src/MainWindow.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using System.Net;
+
+namespace Golden_Apple
+{
+    public partial class MainWindow : Form
+    {
+        private WebClient Client = new WebClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            Client.DownloadProgressChanged += Client_DownloadProgressChanged;
+        }
+        private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+        private async Task BgTask()
+        {
+            while (true)
+            {
+                if (!File.Exists("config.txt"))
+                    File.WriteAllText("config.txt", "0");
+                try
+                {
+                    string Result = Client.DownloadString(new Uri("https://apps.goncermor.com/GoldenApple/Alfa0/update.txt"));
+                    if (Result.Split("<::>")[0] != File.ReadAllText("config.txt"))
+                    {
+                        this.Show();
+                        await Task.Delay(1000);
+                        Client.DownloadFileAsync(new Uri(Result.Split("<::>")[1]), "update.exe");
+                        while (Client.IsBusy)
+                        {
+                            await Task.Delay(50);
+                            Application.DoEvents();
+                        }
+                        await Task.Delay(1000);
+                        File.WriteAllText("config.txt", Result.Split("<::>")[0]);
+                        Process.Start("update.exe");
+                        this.Hide();
+                    }
+                }
+                catch (Exception e)
+                {
+                    this.Hide();
+                }
+                await Task.Delay(5000);
+            }
+        }
+        private void MainWindow_Shown(object sender, EventArgs e)
+        {
+            this.Hide();
+            _ = BgTask();
+        }
+        private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.Hide();
+            e.Cancel = true;
+        }
+    }
+}
